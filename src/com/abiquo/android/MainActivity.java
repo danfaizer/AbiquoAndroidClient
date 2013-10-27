5582cee909f16d71fcfa9d7770dbbd204e816045
dbeneyto <danfaizer@danfaizer-dell.(none)>
2013-10-27 11:41:33 +0100

    Added preferences menu and check 1st time run

--- src/com/abiquo/android/MainActivity.java
+++ src/com/abiquo/android/MainActivity.java
@@ -1,6 +1,10 @@
 package com.abiquo.android;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /*
  * Activities should extend from BaseActivity to allow use custom
@@ -13,11 +17,51 @@ public class MainActivity extends BaseActivity {
 		super.onCreate(savedInstanceState);
 	    
 		setContentView(R.layout.mainactivity);
+	
+		/*
+		 * Check if is 1st time application is run so we
+		 * can show Preferences screen and show a disclaimer
+		 * if is required, etc.
+		 */
+		
+		 // 0 = mode private. Only this App can read these preferences
+		 SharedPreferences appPreferences = this.getSharedPreferences("settings", 0);
+		 if (appPreferences.getBoolean("prefappfirstrun", true)) {
+			 setRunned(appPreferences);
+	         Intent i = new Intent(this, PreferencesActivity.class);            
+	         startActivity(i);	         
+		 }
 
 	}
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.settings, menu);
+        return true;
+    }
+ 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+ 
+        case R.id.menu_settings:
+            Intent i = new Intent(this, PreferencesActivity.class);            
+            startActivity(i);
+            break;
+        }
+ 
+        return true;
+    }
+
+	public void setRunned(SharedPreferences appPreferences) {
+		  SharedPreferences.Editor edit = appPreferences.edit();
+		  edit.putBoolean("prefappfirstrun", false);
+		  edit.commit();
+	}
+    
 	@Override
 	protected int getLayoutResourceId() {
 		return R.layout.mainactivity;
 	}
+	
 }
---
+++ src/com/abiquo/android/PreferencesActivity.java
@@ -0,0 +1,22 @@
+package com.abiquo.android;
+ 
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+ 
+public class PreferencesActivity extends PreferenceActivity {
+ 
+    @SuppressWarnings("deprecation")
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+ 
+        addPreferencesFromResource(R.xml.preferences);
+        
+        /*
+         * Remove app hidden preferences
+         */
+        PreferenceCategory hiddenPreferencesCategory = (PreferenceCategory) findPreference("pref_hidden");
+        hiddenPreferencesCategory.removeAll();
+    }
+}
